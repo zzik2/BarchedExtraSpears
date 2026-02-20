62dbf4a50e5fc8703c2edd26811d23a969f5b50f
zzik2 <cheese.zzik2@gmail.com>
2026-02-20 10:51:23 +0900

    refactor start

---
+++ common/src/main/java/zzik2/barched/extra/spears/BarchedES.java
@@ -0,0 +1,14 @@
+package zzik2.barched.extra.spears;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class BarchedES {
+
+    public static final String MOD_ID = "barched_extra_spears";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
