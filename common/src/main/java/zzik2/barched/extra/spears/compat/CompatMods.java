ec76c7d41be7032051886e9866bb8c0b0c649542
zzik2 <cheese.zzik2@gmail.com>
2026-02-21 18:49:10 +0900

    Progression Reborn mod compat!

--- common/src/main/java/zzik2/barched/extra/spears/compat/CompatMods.java
+++ common/src/main/java/zzik2/barched/extra/spears/compat/CompatMods.java
@@ -2,12 +2,14 @@ package zzik2.barched.extra.spears.compat;
 
 import zzik2.barched.extra.spears.compat.impl.BronzeCompat;
 import zzik2.barched.extra.spears.compat.impl.ParadiseLostCompat;
+import zzik2.barched.extra.spears.compat.impl.ProgressionRebornCompat;
 
 @SuppressWarnings("all")
 public enum CompatMods {
 
     BRONZE(new BronzeCompat()),
     PARADISE_LOST(new ParadiseLostCompat()),
+    PROGRESSION_REBORN(new ProgressionRebornCompat()),
     ;
 
     private final ICompatMod compatMod;
---
+++ common/src/main/java/zzik2/barched/extra/spears/compat/impl/ProgressionRebornCompat.java
@@ -0,0 +1,49 @@
+package zzik2.barched.extra.spears.compat.impl;
+
+import net.legacy.progression_reborn.registry.PRTiers;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import zzik2.barched.Barched;
+import zzik2.barched.extra.spears.compat.ICompatMod;
+import zzik2.barched.extra.spears.data.RegistryData;
+import zzik2.barched.extra.spears.data.creativetab.TabData;
+import zzik2.barched.extra.spears.data.items.SpearAttributeData;
+import zzik2.barched.extra.spears.data.items.SpearData;
+import zzik2.barched.extra.spears.data.material.MaterialData;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class ProgressionRebornCompat implements ICompatMod {
+
+    @Override
+    public String getModID() {
+        return "progression_reborn";
+    }
+
+    @Override
+    public List<RegistryData> getDefaultMaterials() {
+        Supplier<CreativeModeTab> TAB = () -> BuiltInRegistries.CREATIVE_MODE_TAB.get(CreativeModeTabs.COMBAT);
+        Supplier<Item> GOLDEN_SPEAR = () -> Barched.Items.GOLDEN_SPEAR;
+        return List.of(
+                //1.2x stronger than iron
+                new RegistryData(
+                        new MaterialData<>(PRTiers.ROSE, "rose"),
+                        new SpearData<>(new SpearAttributeData(
+                                0.7917F,
+                                1.14F,
+                                0.5F,
+                                3.0F,
+                                6.6667F,
+                                8.1F,
+                                4.25F,
+                                13.5F,
+                                3.8333F
+                        ), null),
+                        new TabData<>(TAB, null, GOLDEN_SPEAR)
+                )
+        );
+    }
+}
